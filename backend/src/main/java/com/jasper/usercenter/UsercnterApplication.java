82853a821adea6d4032597291403c368c529ee27
LarsonJJKing <tingyu.zhang@ucdconnect.ie>
2024-06-06 15:24:52 +0100

    后端初始化

---
+++ backend/src/main/java/com/jasper/usercenter/UsercnterApplication.java
@@ -0,0 +1,13 @@
+package com.jasper.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercnterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercnterApplication.class, args);
+	}
+
+}
